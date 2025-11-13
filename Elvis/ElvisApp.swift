9fe4907aad3b3c3609682a7ae0fabe972b841e60
Julian Jaramillo <thatkidjuju18@icloud.com>
2025-11-13 11:07:11 -0800

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by JARAMILLO, JULIAN on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElvisTests/ElvisTests.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisTests.swift
+//  ElvisTests
+//
+//  Created by JARAMILLO, JULIAN on 11/13/25.
+//
+
+import Testing
+@testable import Elvis
+
+struct ElvisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
